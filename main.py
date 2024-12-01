88aae1befe7707b5e2460f628729c079df1e0af3
asobini <89592607+asobini@users.noreply.github.com>
2024-12-01 22:23:18 +0100

    Update main.py
    unnecessary comments removed

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 
 
-from sqlalchemy.testing.util import random_choices
-
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
-
 #import colorgram
 
 #colors = colorgram.extract('image.jpg', 30)
